3eb721fcb5a6ae627d583d5e43380c6411772544
mario <anserteampro@gmail.com>
2021-10-14 23:07:42 +0700

    Update script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo "Run $0"
 CUR_DIR=`dirname $0`
 cd $CUR_DIR
-python $CUR_DIR/src/crawler.py
+/home/mario/miniconda3/bin/python $CUR_DIR/src/crawler.py
 git add data
 git commit -m "update data"
 git push
